3e397f0f6a99f89bff9903c57c9db289c76ac320
Bernat Borrás <bernatbor15@gmail.com>
2025-09-06 17:09:31 +0200

    Add home screen

--- composeApp/src/commonMain/kotlin/com/alorma/camperrent/App.kt
+++ composeApp/src/commonMain/kotlin/com/alorma/camperrent/App.kt
@@ -1,18 +1,12 @@
 package com.alorma.camperrent
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import androidx.navigation3.runtime.NavEntry
 import androidx.navigation3.ui.NavDisplay
+import com.alorma.camperrent.screen.home.HomeScreen
 import com.alorma.camperrent.screen.home.HomeScreenRoute
 import com.alorma.camperrent.screen.route.ScreenRoute
 import org.jetbrains.compose.ui.tooling.preview.Preview
@@ -40,14 +34,7 @@ fun App() {
           logger.debug(route.logName)
           when (route) {
             is HomeScreenRoute -> NavEntry(route) {
-              Scaffold { paddingValues ->
-                Box(
-                  modifier = Modifier.fillMaxSize().padding(paddingValues),
-                  contentAlignment = Alignment.Center,
-                ) {
-                  Text("Home Screen") // Replace with your home screen content
-                }
-              }
+              HomeScreen()
             }
 
             else -> throw IllegalArgumentException("Unknown route: $route")
--- composeApp/src/commonMain/kotlin/com/alorma/camperrent/screen/home/Home.kt
+++ composeApp/src/commonMain/kotlin/com/alorma/camperrent/screen/home/Home.kt
@@ -1,6 +1,13 @@
 package com.alorma.camperrent.screen.home
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.alorma.camperrent.screen.route.ScreenRoute
 
 data object HomeScreenRoute : ScreenRoute {
@@ -9,5 +16,12 @@ data object HomeScreenRoute : ScreenRoute {
 
 @Composable
 fun HomeScreen() {
-
+  Scaffold { paddingValues ->
+    Box(
+      modifier = Modifier.fillMaxSize().padding(paddingValues),
+      contentAlignment = Alignment.Center,
+    ) {
+      Text("Home Screen") // Replace with your home screen content
+    }
+  }
 }
